19d99c3fc9859510629debe39653bea71abee6aa
Pavel <pasha@gsdghrte.com>
2019-09-07 16:41:38 -0500

    Tip Caclulator V4

--- app/src/main/java/com/missouristate/betamen/a03_tipcalc_v13/MainActivity.java
+++ app/src/main/java/com/missouristate/betamen/a03_tipcalc_v13/MainActivity.java
@@ -2,6 +2,8 @@ package com.missouristate.betamen.a03_tipcalc_v13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -15,20 +17,32 @@ public class MainActivity extends AppCompatActivity {
 
 
     private TipCalculator tipCalc;
-    public NumberFormat money = NumberFormat.getCurrencyInstance( );
+    public NumberFormat money = NumberFormat.getCurrencyInstance();
+
+        // make billEditText and topEditClass for whole java.
+    private EditText billEditText;
+    private EditText tipEditText;
 
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         tipCalc = new TipCalculator( 0.17f, 100.0f );
         setContentView( R.layout.activity_main );
+
+        billEditText = findViewById(R.id.editText_EnterBillAmount);
+        tipEditText = findViewById(R.id.editText_EnterTipAmount);
+
+        //creates a new class of listener
+        TextChangeHandler tch = new TextChangeHandler();
+        //we want to listen to those entries for any changes
+        billEditText.addTextChangedListener(tch);
+        tipEditText.addTextChangedListener(tch);
     }
 
     //Called when the user clicks on the Calculate button
-    public void calculate( View v ) {
-        Log.w( "MainActivity", "v = " + v );
+    public void calculate( ) {
 
-        EditText billEditText = findViewById(R.id.editText_EnterBillAmount);
-        EditText tipEditText = findViewById(R.id.editText_EnterTipAmount);
+
+
 
         // the billEditText is a Class so we need to convert it to the String.
         String billString = billEditText.getText( ).toString( );
@@ -55,6 +69,24 @@ public class MainActivity extends AppCompatActivity {
             totalTextView.setText( money.format( total ) );
         } catch( NumberFormatException nfe ) {
 
+        }
+    }
+
+    private class TextChangeHandler implements TextWatcher {//imolements TextWatcher was added
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            calculate();
         }
     }
 }
